8bdf3333eccfdc169ef35d20eea61753cb1388cb
Keli06001 <keli06001@utb.vaxjo.se>
2023-09-15 13:10:36 +0200

    Add project files.

---
+++ Uppgift2.3/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Uppgift2_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur många dagar ska du hyra bilen?");
+            string dagarText = Console.ReadLine();
+            int dagar=int.Parse(dagarText);
+            Console.WriteLine("Hur många kilometer ska du köra?");
+            string kilometerText = Console.ReadLine();
+            int kilometer = int.Parse(kilometerText);
+            int kostnad = 300 + (dagar-1)*500 + (kilometer*1);
+            Console.WriteLine(kostnad+"kr");
+        }
+    }
+}
